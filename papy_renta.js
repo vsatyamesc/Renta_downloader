4374d983c6cd20a0581e2499eeb25c3be74e27d2
Satyam <60954779+vsatyamesc@users.noreply.github.com>
2025-06-08 15:21:46 +0530

    Update papy_renta.js

--- papy_renta.js
+++ papy_renta.js
@@ -1,5 +1,6 @@
 // Date 19 March 2025
 // You can change start_img_1qa by any number, 0-indexed to make it start from that page, meaning if you want to download from page 10, do 10-1=9, and set it to 9
+// Same for end image, end_img_1qa, don't want to download full chapter set the ending page. same as above, ending page is 24, then set it to 24-1 = 23
 let image_max_1qa = document.getElementById("scrollBar");
 const start_img_1qa = Number(image_max_1qa.min);
 const end_img_1qa = Number(image_max_1qa.max);
